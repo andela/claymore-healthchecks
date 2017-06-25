d35255c9294e3d285bea0128dcd338d668c6835d
DomieBett <dbett49@gmail.com>
2017-06-25 09:58:54 +0300

    [feature #147010553] Add test for checking team access works

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -1,5 +1,6 @@
 from hc.api.models import Check
 from hc.test import BaseTestCase
+from bs4 import BeautifulSoup
 
 
 class AddCheckTestCase(BaseTestCase):
@@ -11,4 +12,14 @@ class AddCheckTestCase(BaseTestCase):
         self.assertRedirects(r, "/checks/")
         assert Check.objects.count() == 1
 
-    ### Test that team access works
+    def test_team_access_works(self):
+        url = "/checks/add/"
+        self.client.login(username="alice@example.org", password="password")
+        self.client.post(url)
+        check = Check.objects.filter()
+        check_code = list(check)[0].code
+
+        url = "/checks/"
+        self.client.login(username="bob@example.org", password="password")
+        response = self.client.post(url)
+        self.assertIn(str(check_code), response.content.decode('utf-8'))
