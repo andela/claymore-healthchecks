ecda557972b6a3d2b7b6328755d64a25d1ae5b13
bryoo <bryantskisseu@gmail.com>
2017-06-27 19:06:18 +0300

    Add send alerts tests

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -30,6 +30,7 @@ class SendAlertsTestCase(BaseTestCase):
         assert set(names) == set(handled_names)
         ### The above assert fails. Make it pass
 
+
     def test_it_handles_grace_period(self):
         check = Check(user=self.alice, status="up")
         # 1 day 30 minutes after ping the check is in grace period:
@@ -37,6 +38,6 @@ class SendAlertsTestCase(BaseTestCase):
         check.save()
 
         # Expect no exceptions--
-        Command().handle_one(check)
-
+        result = Command().handle_one(check)
     ### Assert when Command's handle many that when handle_many should return True
+        assert result is True
