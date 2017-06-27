89c64d0e506502d109e2e9cf8a613b915640c982
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-06-27 11:02:39 +0300

    merge branches test switch team and middelware

---
+++ hc/local_urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+import debug_toolbar
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('hc.accounts.urls')),
+    url(r'^__debug__/', include(debug_toolbar.urls)),
+    url(r'^', include('hc.api.urls')),
+    url(r'^', include('hc.front.urls')),
+    url(r'^', include('hc.payments.urls'))
+]
