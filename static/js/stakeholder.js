1a8bf6166d7f512e541aa0cb3e0f0019a07a86e9
DomieBett <dbett49@gmail.com>
2017-07-08 23:21:12 +0300

    [Feature #147010571] Add Jquery function to show stakeholder modal

--- static/js/stakeholder.js
+++ static/js/stakeholder.js
@@ -3,12 +3,41 @@
  */
 
 $(function () {
+    hierachy = $(".hierachy-value").html();
     $("#add_stakeholder").click(function(e){
         var url = e.target.getAttribute("data-url");
-        // var code = window.location.href.split('/')[5];
-        // $("#stakeholder").attr("action", "/stakeholders/add/"+code+"/");
         $("#stakeholder").attr("action", url);
         $("#update-stakeholder-modal").modal("show");
+        return false;
+    });
+
+    $("#increment-btn").click(function(e){
+        if (hierachy >= 6){
+            $("#increment-btn").attr('disabled', 'disabled');
+        }
+        else if(hierachy < 6){
+            $("#increment-btn").removeAttr('disabled')
+            $(".hierachy-value").html(hierachy++)
+        }
+    });
+
+    $("#decrement-btn").click(function(e){
+        if (hierachy <= 0){
+            $("#decrement-btn").attr('disabled', 'disabled');
+        }
+        else if(hierachy > 0){
+            $("#decrement-btn").removeAttr('disabled');
+            $(".hierachy-value").html(hierachy--)
+        }
+    });
+
+    $(".stake-holder-remove").click(function(e){
+        url = e.target.getAttribute("data-url");
+        name = e.target.getAttribute("data-name");
+        $("#remove-stakeholder-form").attr("action", url);
+        $(".remove-stakeholder-name").text(name)
+
+        $("#remove-stakeholder-modal").modal("show");
         return false;
     });
 });
