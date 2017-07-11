e728e40c81d3f30ad897b169c8fdead754e448e8
DomieBett <dbett49@gmail.com>
2017-07-11 14:38:38 +0300

    [Fix #147010571] Fix bug where stakeholder would not delete Add notification for user if stakeholder added exists.

--- static/js/stakeholder.js
+++ static/js/stakeholder.js
@@ -9,14 +9,13 @@ $(function () {
         $("#update-stakeholder-modal").modal("show");
         return false;
     });
-    
-    $(".stake-holder-remove").click(function(e){
-        url = e.target.getAttribute("data-url");
-        name = e.target.getAttribute("data-name");
-        $("#remove-stakeholder-form").attr("action", url);
-        $(".remove-stakeholder-name").text(name);
 
+    $(".stake-holder-remove").click(function(e){
+        var $this = $(this);
+        $("#remove-stakeholder-form").attr("action", $this.data("url"));
+        $(".remove-stakeholder-name").text($this.data("name"));
         $("#remove-stakeholder-modal").modal("show");
+
         return false;
     });
 });
@@ -36,5 +35,13 @@ function change_hierachy_value(value, st_id){
             return false
         }
     }
-    hierachy_id.html( parseInt(hierachy_value) + value);
+    hierachy_id.html(parseInt(hierachy_value) + value);
+}
+
+function stakeholder_popup(){
+    $("#stakeholder_popup").show();
+    setTimeout(function(){
+        $("#stakeholder_popup").hide(1000);
+    },
+    3000);
 }
